db3e1d2ea0500fc1d4bdde67326569c8a2a034ef
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-05 18:24:02 -0500

    get values for form

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Question = () => {
+
+    // ?1. create state
+    const [amount, setSavedAmount] = useState(0);
+    const [error, setSavedError] = useState(false);
+
+    // ?2 function for get budget
+    const defineBudget = (e) => {
+        setSavedAmount(parseInt(e.target.value, 10));
+    }
+
+    // ?3. Submit for defined the budget
+    const addBudget = (e) => {
+        e.preventDefault();
+
+        // ?4. validation
+        if(amount < 1 || isNaN(amount)) {
+            setSavedError(true);
+            return;
+        }
+
+        // ?5. check validation
+        setSavedError(false);
+    }
+
     return (
         <Fragment>
             <h2>Ingresa tu presupuesto</h2>
 
-            <form>
+            <form onSubmit={addBudget}>
                 <input 
                     type="text"
                     className="u-full-width"
                     placeholder="Ingresa tu presupuesto"
+                    // ?2. define budget
+                    onChange={defineBudget}
                 />
 
                 <input 
